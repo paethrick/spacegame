91a848a79a36268d05ae2988187f58d8688aef5d
paethrick <izelierik9@gmail.com>
2022-03-29 19:27:03 +0200

    shooting added

--- index.js
+++ index.js
@@ -44,7 +44,40 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        
+        this.radius = 3
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'red'
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 const player = new Player()
+const projectiles = [new Projectile({
+    position: {
+        x:0,
+        y:0
+    },
+    velocity :{
+        x:0,
+        y:0
+    }
+})]
 const keys = {
     a: {
         pressed: false
@@ -59,14 +92,24 @@ const keys = {
 
 function animate (){
     requestAnimationFrame(animate)
+    const speed = 5
     c.fillStyle = 'black'
     c.fillRect(0,0,canvas.width,canvas.height)
     player.update()
+    projectiles.forEach((projectile, index) => {
+        if(projectile.position.y + projectile.radius <= 0){
+            setTimeout(() => {
+                projectiles.splice(index,1)
+            }, 0)
+    } else {
+        projectile.update()
+    }
+    })
 
     if (keys.a.pressed && player.position.x >= 0) {
-        player.velocity.x = -5
+        player.velocity.x = - speed
     } else if (keys.d.pressed && player.position.x + player.width <= canvas.width){
-        player.velocity.x = 5
+        player.velocity.x = speed
     } else {
         player.velocity.x = 0
     }
@@ -77,15 +120,27 @@ animate()
 addEventListener('keydown', ({key}) => {
     switch (key) {
         case 'a': 
-            console.log('left')
+            //console.log('left')
             keys.a.pressed = true
             break
         case 'd': 
-            console.log('right')
+            //console.log('right')
             keys.d.pressed = true
             break
         case ' ': 
-            console.log('space')
+            //console.log('space')
+            projectiles.push(new Projectile({
+                position: {
+                    x:player.position.x + player.width / 2,
+                    y:player.position.y
+                },
+                velocity :{
+                    x:0,
+                    y:-10
+                }
+            }))
+
+            console.log(projectiles)
             break
         
     }
