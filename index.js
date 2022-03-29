9a04c450172bbcc45221ba873aae5089bd3a8562
paethrick <izelierik9@gmail.com>
2022-03-29 21:42:20 +0200

    invaders move around

--- index.js
+++ index.js
@@ -100,11 +100,11 @@ class Invader {
         
     }
 
-    update() {
+    update({velocity}) {
         if (this.image) {
         this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
+        this.position.x += velocity.x
+        this.position.y += velocity.y
         }
     }
 }
@@ -117,14 +117,18 @@ class Grid {
         }
 
         this.velocity = {
-            x: 0,
+            x: 5,
             y: 0
         }
 
         this.invaders = []
+        const column = Math.floor(Math.random() * 4 + 5)
+        const row = Math.floor(Math.random() * 4 + 2)
 
-        for (let i=0; i<7; i++){
-            for (let k=0; k<7; k++){
+        this.width = column * 37
+
+        for (let i=0; i<column; i++){
+            for (let k=0; k<row; k++){
             this.invaders.push(
                 new Invader({
                     position: {
@@ -135,7 +139,17 @@ class Grid {
         }}
     }
 
-    update(){}
+    update(){
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        this.velocity.y = 0
+
+        if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+            this.velocity.x = -this.velocity.x
+            this.velocity.y = 30
+        }
+    }
 }
 
 const player = new Player()
@@ -173,7 +187,7 @@ function animate (){
     grids.forEach(grid => {
         grid.update()
         grid.invaders.forEach((invader) => {
-            invader.update()
+            invader.update({velocity: grid.velocity})
         })
     })
 
